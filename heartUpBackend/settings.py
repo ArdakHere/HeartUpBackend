18309085913f105b7d4ea0e775e545281cf96336
Nurbek <nurbek.malikov@nu.edu.kz>
2024-05-04 00:53:53 +0500

    removed security vulnerabilites

--- heartUpBackend/settings.py
+++ heartUpBackend/settings.py
@@ -197,9 +197,9 @@ AUTH_USER_MODEL = 'authentication.User'
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = "info@demomailtrap.com"
-EMAIL_HOST = 'live.smtp.mailtrap.io'
-EMAIL_HOST_USER = 'api'
-EMAIL_HOST_PASSWORD = 'de43358b9aa3f006b8b16abff071f84d'
-EMAIL_PORT = '587'
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True
